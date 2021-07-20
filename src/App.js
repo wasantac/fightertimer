84341f7e9208d643becba4f36a042555c44e9ff2
wasantac <34143200+wasantac@users.noreply.github.com>
2021-07-19 22:04:56 -0500

    outro

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.png';
 import './App.css';
 import {CountdownCircleTimer} from 'react-countdown-circle-timer';
 import {Container,Col,Row} from 'reactstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 const renderTime = ({ remainingTime }) => {
   if (remainingTime === 0) {
     return <h1 className="timer empezando">Empezando</h1>;
@@ -38,31 +43,44 @@ function App() {
     triangles.push(<span className="triangle" key={i}></span>)
   }
   return (
-    <div className="App">
-          {triangles.map(item =>{ 
-            return item
-          })}
-        <Container className="up bg-transparent">
-
-          <Row>
-            <Col sm={6}>
-              <img src={logo} alt="logo" width="auto" height="700rem" className="logoF"></img>
+    <div className="App">\          
+      {triangles.map(item =>{ 
+        return item
+      })}
+      <Router>
+        <Switch>
+          <Route exact path="/">
+          <Container className="up bg-transparent">
+            <Row>
+              <Col sm={6}>
+                <img src={logo} alt="logo" width="auto" height="700rem" className="logoF"></img>
+              </Col>
+              <Col sm={6} className="d-flex align-items-center justify-content-center">
+              <CountdownCircleTimer
+                isPlaying
+                duration={300}
+                size="400"
+                trailColor="$FFFFFF"
+                colors={[["#FEBF1A"],["#F59D1D"],["#F37A1E"]]}
+                onComplete={() => [true,6000000]}
+              >
+                {renderTime}
+              </CountdownCircleTimer>
+              </Col>
+            </Row>
+            </Container>
+          </Route>
+          <Route exact path="/outro">
+            <Col>
+            <img src={logo} alt="logo" width="auto" height="700rem" className="logoF"></img>
+            <h1 className="final">Gracias por ver!</h1>
             </Col>
-            <Col sm={6} className="d-flex align-items-center justify-content-center">
-            <CountdownCircleTimer
-              isPlaying
-              duration={300}
-              size="400"
-              trailColor="$FFFFFF"
-              colors={[["#FEBF1A"],["#F59D1D"],["#F37A1E"]]}
-              onComplete={() => [true,6000000]}
-            >
-              {renderTime}
-            </CountdownCircleTimer>
-            </Col>
-          </Row>
-        </Container>
 
+          </Route>
+
+        </Switch>
+
+      </Router>
     </div>
   );
 }
